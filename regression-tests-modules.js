3af966a76b2ca9f1afe8271ea68e6a1b3a4bebbf
tara_sooo <266087079+tara-sooo@users.noreply.github.com>
2026-06-26 15:31:34 +0900

    Test extracted progression data modules

--- regression-tests-modules.js
+++ regression-tests-modules.js
@@ -7,9 +7,11 @@ const originalReadFileSync = fs.readFileSync.bind(fs);
 const runtimeSource = [
   originalReadFileSync(path.join(root, "src/runtime/core.js"), "utf8"),
   "\n// BEGIN INTEGRATED BALANCE RULES\n",
+  originalReadFileSync(path.join(root, "src/data/progression-definitions.js"), "utf8"),
   originalReadFileSync(path.join(root, "src/data/balance-profile.js"), "utf8"),
   originalReadFileSync(path.join(root, "src/systems/balance-formulas.js"), "utf8"),
   originalReadFileSync(path.join(root, "src/systems/balance-runtime.js"), "utf8"),
+  originalReadFileSync(path.join(root, "src/systems/progression-data-runtime.js"), "utf8"),
 ].join("\n");
 
 fs.readFileSync = (file, options) => {
